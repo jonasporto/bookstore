1a4d6a369482d7ee1268ffe1462f863c882ead3c
Jonas Porto <jporto.contato@hotmail.com>
2016-09-09 20:01:48 -0300

    adding ejs to use in view templates

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.send('<h1>Home</h1>');
+});
+
+app.get('/products', function(request, response) {
+	response.render('products/list');
+});
 
 app.listen(3000, function() {
 	console.log('server running');
