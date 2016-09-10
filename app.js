750bb366a0fc0d6fc0b0b210ab7650469d3a3499
Jonas Porto <jporto.contato@hotmail.com>
2016-09-09 22:13:10 -0300

    reorganizing route files

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('./config/express')();
 
-app.get('/', function(request, response) {
-	response.send('<h1>Home</h1>');
-});
-
 app.listen(3000, function() {
 	console.log('server running');
 });
